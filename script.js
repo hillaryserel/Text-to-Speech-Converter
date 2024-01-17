d8164d9b211719e3e694831efd6c42e17a07d576
Hillary Serel <Serelhillary00@gmail.com>
2024-01-17 13:24:31 +0300

    Full project

---
+++ script.js
@@ -0,0 +1,30 @@
+let speech = new SpeechSynthesisUtterance();
+
+//Array to store available voices
+let voices = [];
+
+// Select the element in HTML
+let voiceSelect =document.querySelector("select");
+
+//event listener for when voice changes
+window.speechSynthesis.onvoiceschanged = () =>{
+
+    //Get all available voices
+    voices = window.speechSynthesis.getVoices();
+    //Set default voice for the SpeechSynthesisUtterrance
+    speech.voice = voices[0];
+    //Populate the voiceSelect dropdown with available voices
+    voices.forEach((voices, i) => (voiceSelect.options[i] = new Option(voices.name, i)));   
+};
+//Event listener for when the user changes the selected voice
+voiceSelect.addEventListener("change", () =>{
+    speech.voice = voices[voiceSelect.value];
+});
+
+//Event listener for when the user clicks the button
+document.querySelector("button").addEventListener("click", () => {
+    //Set the text of the SpeechSynthesisUtterance to the content of the <textarea>
+    speech.text = document.querySelector("textarea").value;
+    //Use speechSynthesis to speak the provided text
+    speechSynthesis.speak(speech);
+});
